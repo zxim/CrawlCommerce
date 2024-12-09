36124a19a9e9e8d95031c36bfdb08dfdfa2355fe
xmin <tlaals44@naver.com>
2024-12-09 15:50:40 +0900

    tsconfig 수정

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,8 @@
-export default function About() {
+export default function AboutPage() {
   return (
     <div>
       <h1>About Page</h1>
-      <p>This is a simple about page for demonstration.</p>
+      <p>This is a simple about page.</p>
     </div>
   );
 }
