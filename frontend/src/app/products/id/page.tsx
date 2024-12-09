b4884abba31374008eeb007ed6e410b607beba2e
xmin <tlaals44@naver.com>
2024-12-09 16:08:35 +0900

    tsconfig.json 수정

---
+++ frontend/src/app/products/id/page.tsx
@@ -0,0 +1,9 @@
+export default function ProductPage() {
+    return (
+      <div>
+        <h1>Product Page</h1>
+        <p>This is a product page for dynamic routing.</p>
+      </div>
+    );
+  }
+  
